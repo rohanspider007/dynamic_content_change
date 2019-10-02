20a2e0ea0a7de1712239cf5795bd12b6328e1248
rohanspider007 <49246535+rohanspider007@users.noreply.github.com>
2019-10-02 22:42:22 +0530

    Update views.py
    card add

--- Ajax/views.py
+++ Ajax/views.py
@@ -5,6 +5,19 @@ def index(request):
     return render(request, 'index.html')
 
 
-def data(request):
-    return render(request, 'dataload.html')
+a = 1
+def add(request):
+    global a
+    if a <= 5:
+        a = a+1
+    price = 50 * a
+    return render(request, 'dataload.html', {'a': a, 'price': price})
+
+def sub(request):
+    global a
+    a = a-1
+    if a <= 1:
+        a = 1
+    price = 50 * a
+    return render(request, 'dataload.html', {'a': a, 'price': price})
 
